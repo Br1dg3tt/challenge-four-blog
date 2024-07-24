239d743d1e199eec04d518768dc51d005f977598
Br1dg3tt <br1dg3tt3v3@gmail.com>
2024-07-24 16:54:54 -0600

    added JSON

--- assets/js/form.js
+++ assets/js/form.js
@@ -2,15 +2,30 @@ const userNameInput = document.querySelector('#user-name');
 const titleInput = document.querySelector('#title');
 const contentInput = document.querySelector('#content');
 
-submitButton.addEventListener('click', function (event) {
-  event.preventDefault();
 
  
+    function saveEntry () {
   const entry = {
     userName: userNameInput.value.trim(),
     title: titleInput.value.trim(),
     content: contentInput.value.trim(),
   };
-
+ 
   localStorage.setItem('entry', JSON.stringify(entry));
+}
+
+function renderEntry () {
+
+    const newEntry = JSON.parse(localStorage.getItem('entry'));
+}
+
+submitButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    saveEntry ();
+    renderEntry();
 });
+
+    function init () {
+        renderEntry;
+    }
+    init();
